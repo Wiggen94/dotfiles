db5f0370ad2b8d92f4b94f3239467bb3d94ae511
Gjermund Wiggen <gjermundwiggen@gmail.com>
2025-12-19 10:02:13 +0000

    NixOS rebuild 2025-12-19 10:02:13

--- hyprpanel-no-bluetooth.nix
+++ hyprpanel-no-bluetooth.nix
@@ -13,6 +13,12 @@ pkgs.hyprpanel.overrideAttrs (oldAttrs: {
     sed -i "/import.*Bluetooth.*from.*bluetooth/d" src/components/bar/layout/coreWidgets.tsx
     sed -i "/bluetooth:.*Bluetooth/d" src/components/bar/layout/coreWidgets.tsx
 
-    echo "Patched bluetooth initialization, menu, and bar component out of HyprPanel"
+    # Patch out bluetooth from dashboard controls - don't remove, just don't import AstalBluetooth
+    sed -i "/import AstalBluetooth from/d" src/components/menus/dashboard/controls/ControlButtons.tsx
+    sed -i "/const bluetoothService = AstalBluetooth/d" src/components/menus/dashboard/controls/ControlButtons.tsx
+    # Remove the BluetoothButton from the index exports
+    sed -i "/export.*BluetoothButton/d" src/components/menus/dashboard/controls/index.tsx
+
+    echo "Patched all bluetooth references out of HyprPanel for VM"
   '';
 })
