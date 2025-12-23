b35327b02bfff1e5dcaf7d0c01462cf8414997a4
Gjermund Høsøien Wiggen <gjewig@sikt.no>
2025-12-23 12:09:53 +0100

    Add catppuccin-plymouth,libnotify,writeShellScript (2 files)

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -22,4 +22,38 @@
     lidSwitch = "suspend";
     lidSwitchExternalPower = "ignore";  # Don't suspend when plugged in with lid closed
   };
+
+  # Low battery notification service
+  systemd.user.services.low-battery-notify = {
+    description = "Low battery notification";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "low-battery-check" ''
+        BATTERY_PATH="/sys/class/power_supply/BAT0"
+        [ -d "$BATTERY_PATH" ] || BATTERY_PATH="/sys/class/power_supply/BAT1"
+        [ -d "$BATTERY_PATH" ] || exit 0
+
+        CAPACITY=$(cat "$BATTERY_PATH/capacity")
+        STATUS=$(cat "$BATTERY_PATH/status")
+
+        if [ "$STATUS" = "Discharging" ]; then
+          if [ "$CAPACITY" -le 10 ]; then
+            ${pkgs.libnotify}/bin/notify-send -u critical "Battery Critical" "Battery at $CAPACITY% - plug in now!"
+          elif [ "$CAPACITY" -le 20 ]; then
+            ${pkgs.libnotify}/bin/notify-send -u normal "Battery Low" "Battery at $CAPACITY%"
+          fi
+        fi
+      '';
+    };
+    wantedBy = [];
+  };
+
+  systemd.user.timers.low-battery-notify = {
+    description = "Check battery level every 2 minutes";
+    timerConfig = {
+      OnBootSec = "1min";
+      OnUnitActiveSec = "2min";
+    };
+    wantedBy = [ "timers.target" ];
+  };
 }
