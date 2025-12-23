e8d8114fd0a4f6dbcf2b1f2a721035531910abc6
Gjermund Høsøien Wiggen <gjewig@sikt.no>
2025-12-23 23:38:37 +0100

    Add makeWrapper,orca-slicer,symlinkJoin; Remove orca-slicer

--- modules/common.nix
+++ modules/common.nix
@@ -702,7 +702,20 @@
 
     # 3D Printing
     pkgs.bambu-studio
-    pkgs.orca-slicer
+    # OrcaSlicer wrapped with zink to fix NVIDIA Wayland preview rendering
+    (pkgs.symlinkJoin {
+      name = "orca-slicer-wrapped";
+      paths = [ pkgs.orca-slicer ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/orca-slicer \
+          --set __GLX_VENDOR_LIBRARY_NAME mesa \
+          --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json \
+          --set MESA_LOADER_DRIVER_OVERRIDE zink \
+          --set GALLIUM_DRIVER zink \
+          --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+      '';
+    })
 
     # Distributed computing
     pkgs.boinc              # BOINC client
