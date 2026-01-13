4d8129f1bef4ecf1ed041cce1e3aa0b15c628829
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-01-13 08:30:09 +0100

    Add hyprlandPlugins; Remove hyprlandPlugins

--- modules/home.nix
+++ modules/home.nix
@@ -955,16 +955,17 @@ in
     ### PLUGINS ###
     ################
 
-    plugin = ${pkgs.hyprlandPlugins.hyprexpo}/lib/libhyprexpo.so
-
-    plugin:hyprexpo {
-        columns = 3
-        gap_size = 8
-        bg_col = ${colors.hypr.base}
-        workspace_method = center current
-        skip_empty = true
-        gesture_distance = 300
-    }
+    # TEMPORARILY DISABLED: hyprexpo plugin not compatible with current Hyprland version
+    # plugin = ${pkgs.hyprlandPlugins.hyprexpo}/lib/libhyprexpo.so
+    #
+    # plugin:hyprexpo {
+    #     columns = 3
+    #     gap_size = 8
+    #     bg_col = ${colors.hypr.base}
+    #     workspace_method = center current
+    #     skip_empty = true
+    #     gesture_distance = 300
+    # }
 
 
     #############################
@@ -1104,8 +1105,8 @@ in
     # Wallpaper picker
     bind = $mainMod SHIFT, W, exec, wallpaper-picker
 
-    # Hyprexpo - workspace overview
-    bind = $mainMod, D, hyprexpo:expo, toggle
+    # Hyprexpo - workspace overview (TEMPORARILY DISABLED)
+    # bind = $mainMod, D, hyprexpo:expo, toggle
 
     # Pyprland scratchpads
     bind = $mainMod, Y, exec, pypr toggle term  # Dropdown terminal
