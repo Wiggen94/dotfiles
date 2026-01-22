84cc8227d272ad43ffc22462a2f25f3188a34807
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-01-22 14:33:12 +0100

    Update config

--- modules/common.nix
+++ modules/common.nix
@@ -314,6 +314,11 @@ in
   hardware.ledger.enable = !isWorkHost;  # Ledger hardware wallet udev rules (disabled on work hosts)
   services.blueman.enable = true;
 
+  # Lemokey keyboard HID access for Lemokey Launcher
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", TAG+="uaccess", TAG+="udev-acl"
+  '';
+
   # Allow passwordless sudo for nixos-rebuild and VPN routing (curitz-vpn split-tunnel)
   security.sudo.extraRules = [
     {
