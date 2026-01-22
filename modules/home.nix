0cab2eaf6eb75fcb476a3da8b8748ca6ba92f914
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-01-22 07:43:15 +0100

    Update config

--- modules/home.nix
+++ modules/home.nix
@@ -1574,7 +1574,7 @@ in
         inner_color = ${colors.hypr.surface0}
         font_color = ${colors.hypr.text}
         fade_on_empty = false
-        placeholder_text = "<span foreground='${colors.text}'>Password...</span>"
+        placeholder_text = Password...
         hide_input = false
         position = 0, -50
         halign = center
