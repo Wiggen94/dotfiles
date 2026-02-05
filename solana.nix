f3e22602492b8b31b3ab96c00fab1ff96e6e673e
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-02-05 13:57:45 +0100

    Add autoPatchelfHook,fetchurl,makeWrapper,openssl,stdenv; Remove fetchurl,makeWrapper,openssl,stdenv,systemd

--- solana.nix
+++ solana.nix
@@ -2,51 +2,64 @@
 
 let
   version = "3.1.8";
-in
-pkgs.stdenv.mkDerivation {
-  pname = "solana-cli";
-  inherit version;
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/anza-xyz/agave/releases/download/v${version}/solana-release-x86_64-unknown-linux-gnu.tar.bz2";
-    sha256 = "1s4ihw5zf9f1wbmzq4yp97i69cg9a8cng8dw7sy60x654y0vnlg9";
+  solana-unwrapped = pkgs.stdenv.mkDerivation {
+    pname = "solana-cli-unwrapped";
+    inherit version;
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/anza-xyz/agave/releases/download/v${version}/solana-release-x86_64-unknown-linux-gnu.tar.bz2";
+      sha256 = "1s4ihw5zf9f1wbmzq4yp97i69cg9a8cng8dw7sy60x654y0vnlg9";
+    };
+
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    buildInputs = [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      pkgs.openssl
+      pkgs.systemd
+    ];
+
+    autoPatchelfIgnoreMissingDeps = [
+      "libsgx_uae_service.so"
+      "libsgx_urts.so"
+      "libOpenCL.so.1"
+    ];
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin $out/share/solana
+
+      # Copy SDK and libs
+      cp -r solana-release/bin/platform-tools-sdk $out/share/solana/ || true
+      cp -r solana-release/bin/perf-libs $out/share/solana/ || true
+
+      # Copy binaries (will be wrapped later)
+      for bin in solana-release/bin/*; do
+        if [ -f "$bin" ] && [ -x "$bin" ]; then
+          cp "$bin" $out/bin/
+        fi
+      done
+      runHook postInstall
+    '';
   };
 
-  nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
-  buildInputs = [
-    pkgs.stdenv.cc.cc.lib
-    pkgs.zlib
-    pkgs.openssl
-    pkgs.systemd
-  ];
-
-  autoPatchelfIgnoreMissingDeps = [
-    "libsgx_uae_service.so"
-    "libsgx_urts.so"
-    "libOpenCL.so.1"
-  ];
-
-  sourceRoot = ".";
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin $out/share/solana
-
-    # Copy SDK and libs
-    cp -r solana-release/bin/platform-tools-sdk $out/share/solana/ || true
-    cp -r solana-release/bin/perf-libs $out/share/solana/ || true
-
-    # Copy and wrap binaries
-    for bin in solana-release/bin/*; do
-      if [ -f "$bin" ] && [ -x "$bin" ]; then
-        name=$(basename "$bin")
-        cp "$bin" $out/bin/
-        wrapProgram $out/bin/$name \
-          --set SBF_SDK_PATH $out/share/solana/platform-tools-sdk/sbf \
-          --set-default SOLANA_INSTALL_DIR "\$HOME/.local/share/solana"
-      fi
+in
+pkgs.symlinkJoin {
+  name = "solana-cli-${version}";
+  paths = [ solana-unwrapped ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    # Remove symlinks and create wrapper scripts
+    for bin in $out/bin/*; do
+      name=$(basename "$bin")
+      rm "$bin"
+      makeWrapper ${solana-unwrapped}/bin/$name $out/bin/$name \
+        --set SBF_SDK_PATH ${solana-unwrapped}/share/solana/platform-tools-sdk/sbf \
+        --run 'export SOLANA_INSTALL_DIR="''${SOLANA_INSTALL_DIR:-$HOME/.local/share/solana}"'
     done
-    runHook postInstall
   '';
 
   meta = with pkgs.lib; {
