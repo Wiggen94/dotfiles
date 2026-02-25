660a5bab14270b97e5fde5bd3c59a1bff6dda248
Gjermund Wiggen <gjermund.wiggen@sikt.no>
2026-02-25 08:21:05 +0100

    Update config

--- hosts/sikt/default.nix
+++ hosts/sikt/default.nix
@@ -3,6 +3,9 @@
 { config, pkgs, lib, ... }:
 
 {
+  # No autologin on work laptop - require password at boot
+  services.displayManager.autoLogin.enable = lib.mkForce false;
+
   # Laptop power management
   services.thermald.enable = true;
   services.power-profiles-daemon.enable = true;
