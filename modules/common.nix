64520d8a2b80efe0c0ef5a52b9a9d50d5ed62b9d
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-03-23 18:55:33 +0100

    Update config

--- modules/common.nix
+++ modules/common.nix
@@ -93,6 +93,8 @@ args+=("-authentication")
 exec ${prev.freerdp}/bin/xfreerdp "''${args[@]}"
 EOF
           chmod +x $out/bin/xfreerdp
+          ln -s $out/bin/xfreerdp $out/bin/freerdp
+          ln -s $out/bin/xfreerdp $out/bin/freerdp3
         '';
       };
     })
