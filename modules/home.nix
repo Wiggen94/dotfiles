4688a566baafb826cae88489be73995cc8add19d
Gjermund Wiggen <gjermund.wiggen@sikt.no>
2026-03-31 08:30:06 +0200

    Update config (2 files)

--- modules/home.nix
+++ modules/home.nix
@@ -657,6 +657,7 @@ in
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
+    gtk4.theme = null;
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
