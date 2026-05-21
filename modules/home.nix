38d798452a54452b04955fce9d37e6dd9ddeddcf
Gjermund Høsøien Wiggen <gjermund.wiggen@sikt.no>
2026-05-21 07:22:10 +0200

    Update config

--- modules/home.nix
+++ modules/home.nix
@@ -1150,9 +1150,8 @@ in
       hl.window_rule({ match = { title = "^Picture-in-Picture$" }, opaque = true, pin = true })
       hl.window_rule({ match = { title = "^Picture in picture$" }, opaque = true, pin = true })
 
-      -- World of Warcraft / TurtleWoW via gamescope
+      -- World of Warcraft
       hl.window_rule({ match = { title = "^World of Warcraft$" }, tile = true })
-      hl.window_rule({ match = { class = "^(gamescope)$" }, fullscreen = true })
 
       -- EDMC Modern Overlay
       hl.window_rule({
